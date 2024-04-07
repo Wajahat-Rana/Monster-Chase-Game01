5f30c493b0772e057b16542e987d3f40ef03652e
Wajahat-Rana <wajahatsaleem59@gmail.com>
2024-04-07 01:35:13 -0700

    Added Monster Animations + Movements

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [HideInInspector]
+       public float speed;
+
+       private Rigidbody2D myBody;
+    // Start is called before the first frame update
+    void Awake(){
+        myBody = GetComponent<Rigidbody2D>();
+        speed = 5f;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        myBody.velocity = new Vector2(speed,myBody.velocity.y);
+    }
+}
