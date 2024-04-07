c00c38298ef62fcb0449fced0ddb610c878003aa
Wajahat-Rana <wajahatsaleem59@gmail.com>
2024-04-07 00:30:16 -0700

    Added Player Movements + Game Scene

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Transform playerTransform;
+    private Vector3 cameraPosition;
+
+[SerializeField]
+    private float maxX = 90f;
+[SerializeField]
+    private float minX = -90f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GameObject.FindWithTag("Player").transform;
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        cameraPosition = transform.position;
+        cameraPosition.x = playerTransform.position.x;
+        if(cameraPosition.x>maxX){
+        cameraPosition.x = maxX;
+        }
+        if(cameraPosition.x<minX){
+        cameraPosition.x = minX;
+        }
+
+        transform.position = cameraPosition;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float movementX;
+    private float movementSpeed = 8f;
+    private SpriteRenderer spRender;
+    private Animator anim;
+    private Rigidbody2D myBody;
+    private string Walk_Status = "Walk";
+    private float jumpForce = 10f;
+    private bool isGrounded;
+
+
+    void Awake(){
+        spRender = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+        myBody = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementX = Input.GetAxisRaw("Horizontal");
+        playerMovement();
+        playerAnimation();
+        
+    }
+    void FixedUpdate()
+    {
+        playerJump();
+    }
+
+    void playerMovement(){
+        transform.position = transform.position + new Vector3(movementX,0,0)*movementSpeed*Time.deltaTime;
+    }
+
+    void playerAnimation(){
+        if(movementX < 0){
+            anim.SetBool(Walk_Status,true);
+            spRender.flipX = true;
+        }
+        else if(movementX > 0){
+            anim.SetBool(Walk_Status,true);
+            spRender.flipX = false;
+        }
+        else if(movementX == 0){
+            anim.SetBool(Walk_Status,false);
+            spRender.flipX = false;
+        }
+    }
+
+    void playerJump(){
+        if(Input.GetButtonDown("Jump")&& isGrounded){
+            isGrounded = false;
+            myBody.AddForce(new Vector2(0f,jumpForce),ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision){
+        if(collision.gameObject.CompareTag("Ground")){
+            isGrounded = true;
+        }
+    }
+    
+}
