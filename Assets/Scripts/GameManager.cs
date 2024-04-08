983a4cd91471e7afd74897147d7ea03c7c9143c5
Wajahat-Rana <wajahatsaleem59@gmail.com>
2024-04-08 08:59:21 -0700

    Added Event And Delegations

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class GameManager : MonoBehaviour
 {
@@ -28,10 +30,27 @@ public class GameManager : MonoBehaviour
        if(instance == null){
          instance = this;
          DontDestroyOnLoad(gameObject);
+         
        }
        else{
          Destroy(gameObject);
        }
+   }
+
+    private void OnEnable()
+   {
+      SceneManager.sceneLoaded += OnLevelFinishedLoading;
+   }
+    private void OnDisable()
+   {
+      SceneManager.sceneLoaded -= OnLevelFinishedLoading;
+      
+   }
+
+   void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode){
+      if(scene.name == "Gameplay"){
+         Instantiate(characters[CharIndex]);
+      }
    }
    
 }
