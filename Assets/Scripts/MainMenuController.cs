017d1f27352e424d851aa138942333ea56411609
Wajahat-Rana <wajahatsaleem59@gmail.com>
2024-04-08 00:29:14 -0700

    Added Scene Toggling + Character Select

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenuController : MonoBehaviour
+{
+   public void playGame(){
+
+    int selectedCharacter = int.Parse(UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name); 
+    SceneManager.LoadScene("Gameplay");
+
+
+   }
+}
